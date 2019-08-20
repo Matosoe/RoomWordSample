0574d2f1ded820f032a9c5700e0bd2e27ce23969
Logon Aluno <logonaluno@local.com>
2019-08-19 22:44:38 -0300

    Versão fim da aula

---
+++ app/src/main/java/br/com/fiap/roomwordsample/WordViewModel.kt
@@ -0,0 +1,22 @@
+package br.com.fiap.roomwordsample
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import br.com.fiap.roomwordsample.dao.WordDao
+import br.com.fiap.roomwordsample.dao.WordRoomDatabase
+import br.com.fiap.roomwordsample.model.Word
+import br.com.fiap.roomwordsample.repository.WordRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application,
+                    val wordRepository: WordRepository): AndroidViewModel(application) {
+
+        val allWords: LiveData<List<Word>> = wordRepository.allWords
+
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        wordRepository.insert(word)
+    }
+}
